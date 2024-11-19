3db23a2b9b2bba568cf73450a4fa890dab812dba
2Dmerlyah55 <00001103412760sp@al.educacao.sp.gov.br>
2024-11-19 11:36:33 -0300

    k

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'charada',
+    'O que é, o que é? Quanto mais se tira maior fica ',
+    'O buraco'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? Anda com os pés na cabeça',
+    'O piolho'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? Tem olho, mas não vê',
+    'A agulha'
+)
+
+criaCartao(
+    'Charada',
+    'O que é, o que é? Tem pernas, mas não anda',
+    'A cadeira'
+)
